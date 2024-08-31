6c0f768d6656b2ddcc7a80cb9aea285c6c027011
Anderson Sprenger <anderson.sprenger@edu.pucrs.br>
2024-08-31 14:44:12 -0300

    bairros to regiao in denuncia

--- frontend/src/pages/Denuncia.js
+++ frontend/src/pages/Denuncia.js
@@ -1,119 +1,3 @@
-// // import React from 'react';
-// // import Alerts from '../components/Alerts';
-// // import CustomButton from '../components/CustomButton';
-
-// // const Main = () => {
-// //     return (
-// //         <>
-
-// //             <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-        
-// //             <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-
-// //         </>
-// //     );
-// // };
-
-// // export default Main;
-
-// import React from 'react';
-// import InputLabel from '@mui/material/InputLabel';
-// import FormControl from '@mui/material/FormControl';
-// import MenuItem from '@mui/material/MenuItem';
-// import Select, { SelectChangeEvent } from '@mui/material/Select';
-// import Button from '@mui/material/Button';
-// import ArrowBackIcon from '@mui/icons-material/ArrowBack';
-
-// const Denuncia = () => {
-//     const [group, setGroup] = React.useState('');
-//     const [type, setType] = React.useState('naturais');
-//     const [subGroup, setSubGroup] = React.useState('');
-//     const [bairro, setBairro] = React.useState('');
-
-//     const handleChange = (event) => {
-//         setGroup(event.target.value);
-//         setSubGroup(event.target.value);
-//         setBairro(event.target.value);
-//     };
-//     const handleType = ({ target }) => {
-//         console.log(target.value);
-
-//         setType(target.value);
-//     };
-
-//     return (
-//         <div style={{ width: '90%', maxWidth: '500px', padding: '0px 0px' }}>
-
-//             <div style={{ textAlign: 'left', paddingLeft:'0px', paddingBottom:'0px', paddingTop:'16px'}}>
-//                 <h2 style={{ color: '#1a7235', textAlign: 'left', margin: '0', padding: '0' }}>Fazer Denúncia</h2>
-//                 <Button sx={{color:'#1a7235', textAlign: 'left', padding: '0px', margin: '0', padding: '0'}} startIcon={<ArrowBackIcon />}>Voltar</Button>
-//             </div>
-
-//             <h3 style={{ color: '#1a7235', textAlign: 'left' }}>Grupo de desastre</h3>
-
-//             <FormControl fullWidth>
-//             <InputLabel id="demo-simple-select-label">Grupo</InputLabel>
-//             <Select labelId="demo-simple-select-label" id="demo-simple-select" value={group} label="Grupo"  onChange={(e) => handleType(e)}>
-//                 <MenuItem value={"naturais"}>Desastres Naturais</MenuItem>
-//                 <MenuItem value={"humanos"}>Desastres Humanos</MenuItem>
-//             </Select>
-//             </FormControl>
-
-            
-//             {
-//                 type == 'naturais' ? (
-//                     <FormControl fullWidth>
-                        
-                    
-//                     <Select labelId="demo-simple-select-label" id="demo-simple-select" value={group} label="Grupo" onChange={handleType}>
-//                         <MenuItem value={10}>Terremoto</MenuItem>
-//                         <MenuItem value={20}>Maremoto</MenuItem>
-//                         <MenuItem value={20}>Erupção Vulcânica</MenuItem>
-//                         <MenuItem value={20}>Deslizamento</MenuItem>
-//                         <MenuItem value={20}>Inundação </MenuItem>
-//                         <MenuItem value={20}>Ciclone </MenuItem>
-//                         <MenuItem value={20}>Tornado </MenuItem>
-//                         <MenuItem value={20}>Furacão </MenuItem>
-//                         <MenuItem value={20}>Granizo </MenuItem>
-//                         <MenuItem value={20}>Tempestade de Raios </MenuItem>
-//                         <MenuItem value={20}>Chuva Intensa </MenuItem>
-//                         <MenuItem value={20}>Vendaval</MenuItem>
-//                     </Select>
-//                     </FormControl>
-//                 ) : (
-                    
-
-//                     <FormControl fullWidth>
-//                     <h3 style={{ color: '#1a7235', textAlign: 'left' }}>Bairro</h3>
-//                     <InputLabel id="demo-simple-select-label">Grupo</InputLabel>
-//                     <Select labelId="demo-simple-select-label" id="demo-simple-select" value={group} label="Grupo" onChange={handleChange}>
-//                         <MenuItem value={10}>Arquipelago</MenuItem>
-//                         <MenuItem value={20}>Partenom</MenuItem>
-//                     </Select>
-//                     </FormControl>
-
-//                 )
-//             }
-//             <FormControl fullWidth>
-//                     <h3 style={{ color: '#1a7235', textAlign: 'left' }}>Bairro</h3>
-//                     <InputLabel id="demo-simple-select-label">Grupo</InputLabel>
-//                     <Select labelId="demo-simple-select-label" id="demo-simple-select" value={group} label="Grupo" onChange={handleChange}>
-//                         <MenuItem value={10}>Arquipelago</MenuItem>
-//                         <MenuItem value={20}>Partenom</MenuItem>
-//                     </Select>
-//                     </FormControl>
-            
-
-            
-//             <div style={{ display: 'flex', justifyContent: 'space-evenly', margin: '16px 0px' }}>
-//                 <Button variant="contained" sx={{width: '120px', backgroundColor: '#1a7235'}}>Enviar</Button>
-//             </div>
-//         </div>
-//     );
-// };
-
-// export default Denuncia;
-
 import React from 'react';
 import InputLabel from '@mui/material/InputLabel';
 import FormControl from '@mui/material/FormControl';
@@ -125,7 +9,7 @@ import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 const Denuncia = () => {
     const [group, setGroup] = React.useState('');
     const [subGroup, setSubGroup] = React.useState('');
-    const [bairro, setBairro] = React.useState('');
+    const [regiao, setRegiao] = React.useState('');
 
     const handleGroupChange = (event) => {
         setGroup(event.target.value);
@@ -136,8 +20,8 @@ const Denuncia = () => {
         setSubGroup(event.target.value);
     };
 
-    const handleBairroChange = (event) => {
-        setBairro(event.target.value);
+    const handleRegiaoChange = (event) => {
+        setRegiao(event.target.value);
     };
 
     const subGroups = {
@@ -164,13 +48,12 @@ const Denuncia = () => {
         ],
     };
 
-    const bairros = [
+    const regioes = [
+        'Zona Norte',
+        'Zona Sul',
+        'Zona Leste',
+        'Centro',
         'Arquipélago',
-        'Partenon',
-        'Moinhos de Vento',
-        'Cidade Baixa',
-        'Boa Vista',
-        // adicione mais bairros conforme necessário
     ];
 
     return (
@@ -214,19 +97,19 @@ const Denuncia = () => {
                 </Select>
             </FormControl>
 
-            <h3 style={{ color: '#1a7235', textAlign: 'left', marginTop: '16px' }}>Bairro</h3>
+            <h3 style={{ color: '#1a7235', textAlign: 'left', marginTop: '16px' }}>Região</h3>
             <FormControl fullWidth>
-                <InputLabel id="bairro-select-label">Bairro</InputLabel>
+                <InputLabel id="regiao-select-label">Região</InputLabel>
                 <Select
-                    labelId="bairro-select-label"
-                    id="bairro-select"
-                    value={bairro}
-                    label="Bairro"
-                    onChange={handleBairroChange}
+                    labelId="regiao-select-label"
+                    id="regiao-select"
+                    value={regiao}
+                    label="Região"
+                    onChange={handleRegiaoChange}
                 >
-                    {bairros.map((bairro, index) => (
-                        <MenuItem key={index} value={bairro}>
-                            {bairro}
+                    {regioes.map((regiao, index) => (
+                        <MenuItem key={index} value={regiao}>
+                            {regiao}
                         </MenuItem>
                     ))}
                 </Select>
