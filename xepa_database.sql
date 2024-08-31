10360d5f91655aa6e1765d235a7f7f9dba939e44
lucasbrentano <lucasbrentano@gmail.com>
2024-08-31 05:41:17 -0300

    Adicionado SQL Database.

---
+++ xepa_database.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE alertas (
+    id VARCHAR(255) NOT NULL,
+    mensagem VARCHAR(255),
+    endereco VARCHAR(255),
+    data_hora TIMESTAMP 
+);
+
+CREATE DATABASE denuncias (
+    id VARCHAR(255) NOT NULL,
+    govbr VARCHAR(255) NOT NULL,
+    descricao VARCHAR(255),
+    endereco VARCAHR(255),
+    data_hora TIMESTAMP
+);
+
+CREATE DATABASE rotas (
+    id VARCHAR(255) NOT NULL,
+    trajeto VARCHAR(255),
+    endereco VARCHAR(255)
+);
