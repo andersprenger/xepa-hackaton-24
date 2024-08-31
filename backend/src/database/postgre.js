2fd8a7cb1843c19df7128b30233655eb2f500a0d
Piedro Rockembach <piedrorn@gmail.com>
2024-08-31 04:49:32 -0300

    create database

--- backend/src/database/postgre.js
+++ backend/src/database/postgre.js
@@ -1,5 +1,16 @@
 const{ Client } = require("pg");
-const client = new Client()
+const client = new Client({
+    user: "xepa",
+    database: "xepa_database",
+    host: "localhost",
+    password: "123456",
+    port: 5432,
+})
+/*
+POSTGRES_DB: xepa_database        # Nome do banco de dados padrão
+      POSTGRES_USER: xepa          # Nome de usuário do banco de dados
+      POSTGRES_PASSWORD: 123456 
+*/
 const connectDatabase = async () => {
     await client.connect()
  
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,4 +1,6 @@
 const app = require("./app.js")
+const createDatabase = require("./database/postgre.js");
+createDatabase();
 app.get("/helthcheck", (_req, res) => res.send("OK"));
 
 app.listen(3000, () => { console.log("listening on port 3000")})
